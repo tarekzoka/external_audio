a86cac474c78f59f029b005e1672f88f3b1e9b4a
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2026-09-18 01:14:07 +0300

    Update external-audio.sh

--- external-audio.sh
+++ external-audio.sh
@@ -14,7 +14,7 @@ ARCH=""
 PY_VER=""
 TAR_GZ=""
 PLUGIN_NAME="external_audio"
-BASE_URL="https://github.com/tarekzoka/external_audio/raw/refs/heads/main/external_audio"
+BASE_URL="https://raw.githubusercontent.com/tarekzoka/external_audio/refs/heads/main/external-audio.sh"
 
 welcome_message() {
     echo -e "${CYAN}##########################################${RESET}"
